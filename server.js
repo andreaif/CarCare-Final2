45d0e0466180402726205cb165611610e7f487cd
Andrem <andreaif4495@gmail.com>
2019-05-01 18:45:35 -0400

    heroku

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const users = require('./routes/api/users');
-const carprofile = require('./routes/api/carprofile');
-
+const users = require("./routes/api/users");
+const carprofile = require("./routes/api/carprofile");
 
 const app = express();
 
@@ -13,25 +12,25 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 //Connect to MongoDB
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log('MongoDB Connected'))
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 //Passport middleware
 app.use(passport.initialize());
 
 //Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 //Use routes
-app.use('/api/users', users);
-app.use('/api/carprofile', carprofile);
+app.use("/api/users", users);
+app.use("/api/carprofile", carprofile);
 
 //Port
-const port = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
